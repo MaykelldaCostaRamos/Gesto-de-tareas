7f421d037a3975a1a60fa685a30909ee6c4dbc8d
TheBos-s <mkpako2009@gmail.com>
2025-10-12 20:59:05 +0200

    Creando estructura Projects.js

---
+++ backend/src/models/Projects.js
@@ -0,0 +1,24 @@
+import mongoose from "mongoose";
+
+const projectSchema = new mongoose.Schema({
+  name: {
+    type: String,
+    required: true,
+    trim: true
+  },
+  ownerId: {
+    type: mongoose.Schema.Types.ObjectId,
+    ref: "User",       // referencia al modelo User
+    required: true
+  },
+  collaborators: [
+    {
+      type: mongoose.Schema.Types.ObjectId,
+      ref: "User"      // referencia a otros usuarios que colaboran
+    }
+  ]
+}, { timestamps: true });
+
+const Project = mongoose.model("Project", projectSchema);
+
+export default Project;
