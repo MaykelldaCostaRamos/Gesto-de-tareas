66c252d707cb40cfb71539c225b916012f96e84b
TheBos-s <mkpako2009@gmail.com>
2025-10-21 10:40:22 +0200

    Fix Tailwind content paths for production

--- frontend/vite.config.js
+++ frontend/vite.config.js
@@ -3,7 +3,12 @@ import react from '@vitejs/plugin-react'
 import tailwindcss from '@tailwindcss/vite'
 
 export default defineConfig({
-  plugins: [react(), tailwindcss()],
+  plugins: [react(), tailwindcss({
+    content: [
+        './index.html',
+        './src/**/*.{js,jsx,ts,tsx}'
+      ]
+  })],
   server: {
     proxy: {
       "/api": "http://localhost:4000",
