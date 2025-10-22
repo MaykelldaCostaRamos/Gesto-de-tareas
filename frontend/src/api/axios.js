a660349acbd590e143490b5ca3e5ee9615c365ed
TheBos-s <mkpako2009@gmail.com>
2025-10-22 20:04:21 +0200

    prueba v1.9

--- frontend/src/api/axios.js
+++ frontend/src/api/axios.js
@@ -1,7 +1,7 @@
 import axios from "axios";
 
 const api = axios.create({
-  baseURL: "https://gesto-de-tareas-frontend.onrender.com/api", 
+  baseURL: "https://gestor-de-tareas-1.onrender.com/api", 
   withCredentials: true, 
   headers: {
     "Content-Type": "application/json",
