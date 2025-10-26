0c6190c9a9c88699dccb9f96c02e1802d8a4e6d3
TheBos-s <mkpako2009@gmail.com>
2025-10-26 05:37:22 +0100

    Agregar API a la ruta de projectService

--- frontend/src/api/projectService.js
+++ frontend/src/api/projectService.js
@@ -4,31 +4,31 @@ import api from "./axios";
 
 // Obtener todos los proyectos del usuario
 export const getProjects = async () => {
-  const response = await api.get("/project");  
+  const response = await api.get("/api/project");  
   return response.data;
 };
 
 // Obtener proyecto específico con tareas
 export const getProject = async (projectId) => {
-  const response = await api.get(`/project/${projectId}`);
+  const response = await api.get(`/api/project/${projectId}`);
   return response.data;
 };
 
 // Crear nuevo proyecto
 export const createProject = async (name, description) => {
-  const response = await api.post("/project", { name, description });
+  const response = await api.post("/api/project", { name, description });
   return response.data;
 };
 
 // Editar proyecto
 export const updateProject = async (projectId, name, description) => {
-  const response = await api.put(`/project/${projectId}`, { name, description });
+  const response = await api.put(`/api/project/${projectId}`, { name, description });
   return response.data.project;
 };
 
 // Eliminar proyecto
 export const deleteProject = async (projectId) => {
-  const response = await api.delete(`/project/${projectId}`);
+  const response = await api.delete(`/api/project/${projectId}`);
   return response.data;
 };
 
@@ -36,24 +36,24 @@ export const deleteProject = async (projectId) => {
 
 // Crear nueva tarea en un proyecto
 export const createTask = async (projectId, title, description = "") => {
-  const response = await api.post(`/task/${projectId}`, { title, description});
+  const response = await api.post(`/api/task/${projectId}`, { title, description});
   return response.data.data;
 };
 
 // Cambiar estado de una tarea (pendiente <-> completada)
 export const toggleTaskState = async (taskId) => {
-  const response = await api.put(`/task/${taskId}/toggle`);
+  const response = await api.put(`/api/task/${taskId}/toggle`);
   return response.data.data;
 };
 
 // Actualizar tarea por ID
 export const updateTask = async (taskId, data) => {
-  const response = await api.put(`/task/${taskId}`, data);
+  const response = await api.put(`/api/task/${taskId}`, data);
   return response.data.data;
 };
 
 // Eliminar tarea por ID
 export const deleteTask = async (taskId) => {
-  const response = await api.delete(`/task/${taskId}`);
+  const response = await api.delete(`/api/task/${taskId}`);
   return response.data;
 };
