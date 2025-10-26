406c3e006b368c9c6b6c89e6dfdfc356f7d50bf2
TheBos-s <mkpako2009@gmail.com>
2025-10-26 05:31:31 +0100

    Agregar API a la ruta de authService

--- frontend/src/api/authService.js
+++ frontend/src/api/authService.js
@@ -2,13 +2,13 @@ import api from "./axios";
 
 // Registro (no devuelve token)
 export const registerUser = async (data) => {
-  const response = await api.post("/auth/register", data);
+  const response = await api.post("/api/auth/register", data);
   return response.data;
 };
 
 // Login devuelve token
 export const loginUser = async ({ email, password }) => {
-  const res = await fetch(`${import.meta.env.VITE_API_URL}/auth/login`, {
+  const res = await fetch(`${import.meta.env.VITE_API_URL}/api/auth/login`, {
     method: "POST",
     headers: { "Content-Type": "application/json" },
     credentials: "include",
@@ -24,12 +24,12 @@ export const loginUser = async ({ email, password }) => {
 };
 
 export const logoutUser = async () => {
-  const response = await api.post("/auth/logout");
+  const response = await api.post("/api/auth/logout");
   console.log("Saliendo de la APP: ", response.data);
   return response.data;
 };
 
 export const getProfile = async () => {
-  const response = await api.get("/auth/profile");
+  const response = await api.get("/api/auth/profile");
   return response.data;
 };
